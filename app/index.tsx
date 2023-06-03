6f7f0df355dee6e9d723790afbc75ef3b750ccb0
Kevin Gelpes <31427153+kgelpes@users.noreply.github.com>
2023-06-03 14:16:27 -0300

    Add zoom options, delete dead code

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,10 @@
-import { useEffect, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import {
   ActivityIndicator,
   Button,
@@ -13,15 +19,76 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import {
   Camera,
   CameraPermissionStatus,
+  CameraRuntimeError,
+  PhotoFile,
   useCameraDevices,
 } from "react-native-vision-camera";
+import { TapGestureHandler } from "react-native-gesture-handler";
+import Reanimated, {
+  withSpring,
+  useSharedValue,
+  useAnimatedProps,
+} from "react-native-reanimated";
+import { MAX_ZOOM_FACTOR } from "../Constants";
+import Stack from "../components/Stack";
+
+const ReanimatedCamera = Reanimated.createAnimatedComponent(Camera);
+Reanimated.addWhitelistedNativeProps({
+  zoom: true,
+});
 
 export default function App() {
+  const camera = useRef<Camera>(null);
+  const [isCameraInitialized, setIsCameraInitialized] = useState(false);
+  const zoom = useSharedValue(1);
+  const isPressingButton = useSharedValue(false);
+  const [selectedZoomOption, setSelectedZoomOption] = useState(zoom.value);
+
+  // TODO: check if camera page is active
+  const isActive = true;
+
+  const [cameraPosition, setCameraPosition] = useState<"front" | "back">(
+    "back"
+  );
+  const [flash, setFlash] = useState<"off" | "on">("off");
+
+  // camera format settings
   const devices = useCameraDevices();
-  const device = devices.back;
+  const device = devices[cameraPosition];
+
   const [cameraPermission, setCameraPermission] = useState<
     CameraPermissionStatus | undefined
   >();
+
+  //#region Memos
+
+  const supportsCameraFlipping = useMemo(
+    () => devices.back != null && devices.front != null,
+    [devices.back, devices.front]
+  );
+  //#endregion
+
+  const zoomOptions = useMemo(() => {
+    const options = [] as {
+      value: number;
+      label: string;
+    }[];
+    if (device == null) return options;
+    if (device.minZoom < device.neutralZoom) {
+      options.push({
+        value: device.minZoom,
+        label: "0.5",
+      });
+    }
+    options.push({
+      value: device.neutralZoom,
+      label: "1",
+    });
+    options.push({ value: device.neutralZoom * 1.5, label: "2" });
+    options.push({ value: device.neutralZoom * 2.5, label: "3" });
+
+    return options;
+  }, [device?.minZoom, device?.neutralZoom, device?.maxZoom]);
 
   useEffect(() => {
     const updateCurrentStatus = async () => {
@@ -32,14 +99,6 @@ export default function App() {
     updateCurrentStatus();
   }, []);
 
-  useEffect(() => {
-    const getAvailableCameraDevices = async () => {
-      const aval = await Camera.getAvailableCameraDevices();
-      console.log(aval);
-    };
-    getAvailableCameraDevices();
-  }, []);
-
   const onRequestPermission = async () => {
     const newCameraPermission = await Camera.requestCameraPermission();
     if (newCameraPermission !== "authorized") {
@@ -48,6 +107,66 @@ export default function App() {
       );
     }
   };
+
+  //#region Animated Zoom
+  // This just maps the zoom factor to a percentage value.
+  // so e.g. for [min, neutr., max] values [1, 2, 128] this would result in [0, 0.0081, 1]
+  const minZoom = device?.minZoom ?? 1;
+  const maxZoom = Math.min(device?.maxZoom ?? 1, MAX_ZOOM_FACTOR);
+
+  const cameraAnimatedProps = useAnimatedProps(() => {
+    const z = Math.max(Math.min(zoom.value, maxZoom), minZoom);
+    return {
+      zoom: z,
+    };
+  }, [maxZoom, minZoom, zoom]);
+  //#endregion
+
+  //#region Callbacks
+  const setIsPressingButton = useCallback(
+    (_isPressingButton: boolean) => {
+      isPressingButton.value = _isPressingButton;
+    },
+    [isPressingButton]
+  );
+  // Camera callbacks
+  const onError = useCallback((error: CameraRuntimeError) => {
+    console.error(error);
+  }, []);
+  const onInitialized = useCallback(() => {
+    console.log("Camera initialized!");
+    setIsCameraInitialized(true);
+  }, []);
+  const onMediaCaptured = useCallback((media: PhotoFile, type: "photo") => {
+    console.log(`Media captured! ${JSON.stringify(media)}`);
+    // navigation.navigate("MediaPage", {
+    //   path: media.path,
+    //   type: type,
+    // });
+  }, []);
+  const onFlipCameraPressed = useCallback(() => {
+    setCameraPosition((p) => (p === "back" ? "front" : "back"));
+  }, []);
+  const onFlashPressed = useCallback(() => {
+    setFlash((f) => (f === "off" ? "on" : "off"));
+  }, []);
+  //#endregion
+
+  //#region Tap Gesture
+  const onDoubleTap = useCallback(() => {
+    onFlipCameraPressed();
+  }, [onFlipCameraPressed]);
+  //#endregion
+
+  if (device != null) {
+    console.log(
+      `Re-rendering camera page with ${
+        isActive ? "active" : "inactive"
+      } camera. ` + `Device: "${device.name}"`
+    );
+  } else {
+    console.log("re-rendering camera page without active camera");
+  }
 
   if (!cameraPermission || !device) {
     // Camera permissions are still loading
@@ -86,8 +205,12 @@ export default function App() {
           paddingVertical: 20,
         }}
       >
-        <TouchableOpacity onPress={() => console.log("flash")}>
-          <Ionicons name="flash-outline" size={24} color="white" />
+        <TouchableOpacity onPress={onFlashPressed}>
+          <Ionicons
+            name={flash === "on" ? "flash" : "flash-off"}
+            color="white"
+            size={24}
+          />
         </TouchableOpacity>
         <Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>
           TrueSnap
@@ -96,19 +219,35 @@ export default function App() {
           <Ionicons name="person-outline" size={24} color="white" />
         </TouchableOpacity>
       </View>
-      <Camera
-        style={{
-          width: "100%",
-          aspectRatio: 3 / 4,
-          borderRadius: 12,
-        }}
-        device={device}
-        isActive={true}
-        preset="photo"
-        enableHighQualityPhotos
-        enableZoomGesture
-        photo
-      />
+      {device != null && (
+        <Reanimated.View
+          style={{
+            width: "100%",
+            aspectRatio: 3 / 4,
+            borderRadius: 12,
+          }}
+        >
+          <TapGestureHandler onEnded={onDoubleTap} numberOfTaps={2}>
+            <ReanimatedCamera
+              ref={camera}
+              style={{
+                width: "100%",
+                aspectRatio: 3 / 4,
+                borderRadius: 12,
+              }}
+              device={device}
+              enableHighQualityPhotos
+              isActive={isActive}
+              onInitialized={onInitialized}
+              onError={onError}
+              enableZoomGesture={false}
+              animatedProps={cameraAnimatedProps}
+              photo
+              orientation="portrait"
+            />
+          </TapGestureHandler>
+        </Reanimated.View>
+      )}
 
       <View
         style={{
@@ -120,18 +259,60 @@ export default function App() {
           paddingVertical: 20,
         }}
       >
+        <Stack
+          spacing={12}
+          style={{
+            position: "absolute",
+            top: -20,
+            right: 0,
+            left: 0,
+            zIndex: 10,
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {zoomOptions.map((d, i) => (
+            <TouchableOpacity
+              key={d.value}
+              onPress={() => {
+                setSelectedZoomOption(d.value);
+                zoom.value = withSpring(d.value, {
+                  damping: 20,
+                  mass: 0.5,
+                });
+              }}
+              style={{
+                backgroundColor: "rgba(255,255,255,0.5)",
+                borderRadius: 99,
+                height: selectedZoomOption === d.value ? 32 : 24,
+                width: selectedZoomOption === d.value ? 32 : 24,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 10,
+                  fontWeight: "bold",
+                }}
+              >
+                {`${d.label}${selectedZoomOption === d.value ? "x" : ""}`}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </Stack>
         <TouchableOpacity onPress={() => console.log("gallery")}>
           <Ionicons name="images-outline" size={24} color="white" />
         </TouchableOpacity>
-        <CircleButton
-          onPress={() =>
-            console.log(
-              device?.formats.find((f) => f.isHighestPhotoQualitySupported)
-            )
-          }
-        />
-        <TouchableOpacity onPress={() => console.log("switch to front camera")}>
-          <Ionicons name="sync-outline" size={24} color="white" />
+        <CircleButton onPress={() => console.log(device.neutralZoom)} />
+
+        <TouchableOpacity
+          onPress={onFlipCameraPressed}
+          disabled={!supportsCameraFlipping}
+        >
+          <Ionicons name="camera-reverse" color="white" size={24} />
         </TouchableOpacity>
       </View>
     </SafeAreaView>
